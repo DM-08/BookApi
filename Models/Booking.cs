af34536d1f7d2a4c294af356310a8350f12d177f
Darren <darren.moore011@gmail.com>
2024-09-17 20:00:57 +0100

    Model

---
+++ Models/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace BookApi.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public DateOnly BookingDate { get; set; }
+    }
+}
+
