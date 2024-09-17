4521938089c4bf5822abc8ae9b936091502aa823
Darren <darren.moore011@gmail.com>
2024-09-17 19:53:00 +0100

    1st modifications

---
+++ Controllers/BookingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    public class BookingsController : Controller
+    {
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        [HttpPost]
+        public IActionResult CreateBooking()
+        {
+            BookingsController.Id = 123;
+            return CreatedAtAction(nameof(GetBooking), new { id = BookingsController.Id }, booking);
+        }
+    }
+}
