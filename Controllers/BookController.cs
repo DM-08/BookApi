414d9aaf3a1209f1427a945eddb24e49f4b9d4f2
Darren <darren.moore011@gmail.com>
2024-09-18 19:34:14 +0100

    Controller updates

---
+++ Controllers/BookController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookRepository;
+
+        public BooksController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var books = _bookRepository.GetBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookRepository.GetBookById(id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook(Book book)
+        {
+            _bookRepository.AddBook(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _bookRepository.GetBookById(id);
+            if (book == null)
+                return NotFound();
+
+            _bookRepository.DeleteBook(id);
+            return NoContent();
+        }
+    }
+}
